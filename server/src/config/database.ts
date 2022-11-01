c99a53f92d9bd8e6769acaa602c4f6103d2d7c36
caseymackrell <116777676+caseymackrell@users.noreply.github.com>
2022-11-01 19:52:03 -0400

    started login routes

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,7 +1,15 @@
+export default{
+    port: 3000,
+    host: 'local',
+    dbUri: 'mongodb://localhost:3000/'
+}
+
+
+
 import mongoose from 'mongoose'
 
 
-const URI = process.env.MONGODB_URL
+const URI = process.env.MONGODB_URI
 
 
 mongoose.connect('URI')
